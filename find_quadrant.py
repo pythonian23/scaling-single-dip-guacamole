894e2e913f2a176d09ade219d843af7be0ff7665
pythonian23 <63051526+pythonian23@users.noreply.github.com>
2022-06-17 15:19:28 +0900

    Update find_quadrant.py

--- find_quadrant.py
+++ find_quadrant.py
@@ -1,3 +1,4 @@
+# Find which quadrant a coordinate is in. Anything with 0 is undefined because I'm lazy :/
 get_quadrant = lambda coord: (int(coord[0] > 0)^int(coord[1] > 0))+2*int(coord[1]<0)+1
 
 print(get_quadrant((1,1)))
