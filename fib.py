b33790026f68f04f8fe33682708daf7fb095cd57
pythonian23 <63051526+pythonian23@users.noreply.github.com>
2022-06-17 15:43:50 +0900

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,2 +1,2 @@
-# Recurse for a fibonacci sequence
+# Recurse for a fibonacci sequence but in reverse order
 _fib = lambda x, y, rem: _fib(y, x+y, rem-1) if rem != 1 else x; fib = lambda lim: _fib(1,1,lim)
